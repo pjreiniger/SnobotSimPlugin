1021f5b4f75c033a16aaa271deb72b8afe4f3a8e
PJ Reiniger <pj.reiniger@gmail.com>
2017-12-06 21:21:25 -0500

    Starting to add ability to simulate from the command line

--- src/main/groovy/com/snobot/simulator/plugin/SnobotSimPlugin.groovy
+++ src/main/groovy/com/snobot/simulator/plugin/SnobotSimPlugin.groovy
@@ -2,12 +2,8 @@ package com.snobot.simulator.plugin
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-
-
-class SnobotSimPluginExtension {
-
-}
-
+import org.gradle.api.Task
+import org.gradle.internal.os.OperatingSystem
 
 
 class SnobotSimPlugin implements Plugin<Project> {
@@ -29,38 +25,143 @@ class SnobotSimPlugin implements Plugin<Project> {
         def cscore = "+"
         def opencv = "+"
         
-
-        project.dependencies.ext.snobotSimCompile = {
-            def l = [
-                "com.snobot.simulator:snobot_sim_gui:${snobotSimVersion}",
-                "com.snobot.simulator:snobot_sim_java:${snobotSimVersion}",
-                "com.snobot.simulator:snobot_sim_utilites:${snobotSimVersion}",
-                "com.snobot.simulator:snobot_sim_java:${snobotSimVersion}:uber_native-linux",
-                "com.snobot.simulator:snobot_sim_java:${snobotSimVersion}:uber_native-windows",
-            ]
-
-            l
+        project.configurations.maybeCreate("snobotSimLibs")
+        project.dependencies.add("snobotSimLibs", "com.snobot.simulator:snobot_sim_gui:${snobotSimVersion}:all")
+        project.dependencies.add("snobotSimLibs", "jfree:jcommon:${jfreecommon}")
+        project.dependencies.add("snobotSimLibs", "jfree:jfreechart:${jfreechart}")
+        project.dependencies.add("snobotSimLibs", "log4j:log4j:${log4j}")
+        project.dependencies.add("snobotSimLibs", "org.yaml:snakeyaml:${snakeyaml}")
+        project.dependencies.add("snobotSimLibs", "com.miglayout:miglayout-swing:${miglayout}")
+        project.dependencies.add("snobotSimLibs", "com.miglayout:miglayout-core:${miglayout}")
+        project.dependencies.add("snobotSimLibs", "edu.wpi.first.ntcore:ntcore-jni:${ntcore}:all")
+        project.dependencies.add("snobotSimLibs", "edu.wpi.first.cscore:cscore-jni:${cscore}:all")
+        project.dependencies.add("snobotSimLibs", "org.opencv:opencv-jni:${opencv}:all")
+        project.dependencies.add("snobotSimLibs", "net.java.jinput:jinput:2.0.7")
+        project.dependencies.add("snobotSimLibs", "net.java.jutils:jutils:1.0.0")
+        
+        if (OperatingSystem.current().isWindows()) 
+        {
+            project.dependencies.add("snobotSimLibs", "com.snobot.simulator:snobot_sim_java:${snobotSimVersion}:uber_native-windows")
+        }
+        else 
+        {
+            project.dependencies.add("snobotSimLibs", "com.snobot.simulator:snobot_sim_java:${snobotSimVersion}:uber_native-linux")
         }
 
-        project.dependencies.ext.snobotSimRuntime = {
+        // Configuration for pulling in everything needed for SnobotSim
+        project.dependencies.ext.snobotSimCompile = {
             def l = [
-                "jfree:jcommon:${jfreecommon}",
-                "jfree:jfreechart:${jfreechart}",
-                "log4j:log4j:${log4j}",
-                "org.yaml:snakeyaml:${snakeyaml}",
-                "com.miglayout:miglayout-swing:${miglayout}",
-                "edu.wpi.first.ntcore:ntcore-jni:${ntcore}:all",
-                "edu.wpi.first.cscore:cscore-jni:${cscore}:all",
-                "org.opencv:opencv-jni:${opencv}:all",
+                project.configurations.getByName("snobotSimLibs").dependencies.each {
+                    it
+                }
             ]
 
             l
         }
         
-        project.task('hello') {
-            doLast {
-                println 'Hello from the GreetingPlugin'
+        createSimulateTask(project)
+        updateIdePlugins(project)
+    }
+    
+    void createSimulateTask(Project project)
+    {
+        // Task to run the simulator from the cmd line
+        project.task("simulate") { Task task ->
+            task.group = "SnobotSimulator"
+            task.description = "Launch the simulator"
+
+            task.doLast {
+            
+                List<String> args = getRunArguments(project)
+                println "The command"
+                args.each{
+                    print it + " "
+                }
+                println ""
+
+
+                
+                //println args
+                //println "DDDDDDDD"
+                
+                //def config = project.configurations.getByName("wpiTools")
+                //Set<File> jarfiles = config.files(config.dependencies.find { d -> d.name == "SmartDashboard" })
+                //ProcessBuilder builder
+                //if (OperatingSystem.current().isWindows()) {
+                //    builder = new ProcessBuilder(
+                //        "cmd", "/c", "start",
+                //        "java", "-jar", "${jarfiles.first().absolutePath}".toString()
+                //    )
+                //} else {
+                //    builder = new ProcessBuilder(
+                //        Jvm.current().getExecutable("java").absolutePath,
+                //        "-jar",
+                //        jarfiles.first().absolutePath
+                //    )
+                //}
+                //
+                //builder.directory(smartDashboardDirectory())
+                //builder.start()
             }
         }
     }
+    
+    void updateIdePlugins(Project project)
+    {
+        // Update eclipse to correctly use the jinput natives
+        if(project.hasProperty("eclipse")) {
+            project.eclipse.classpath.file {
+                withXml {
+                    provider ->
+                    provider.asNode().findAll { it.@path.contains("jinput") && !it.@path.contains("natives") }.each {
+                            def container = it
+                            container.appendNode('attributes').appendNode('attribute', [name: 'org.eclipse.jdt.launching.CLASSPATH_ATTR_LIBRARY_PATH_ENTRY', value:project.name + "/build/native_libs"])
+                    }
+                }
+            }
+        }
+        else {
+            println "Eclipse not configured, will not update classpath"
+        }
+    }
+    
+    List<String> getRunArguments(Project project)
+    {
+        def snobotSimLibsConfig = project.configurations.getByName("snobotSimLibs")
+        def compileConfig = project.configurations.getByName("compile")
+        File simulatorJar = snobotSimLibsConfig.files(snobotSimLibsConfig.dependencies.find { d -> d.name == "snobot_sim_gui" }).first()
+        
+        // Get Classpath
+        def classpath = ""
+        classpath = addToClasspath(snobotSimLibsConfig, classpath)
+        classpath = addToClasspath(compileConfig, classpath)
+        classpath = classpath[ 0..-2 ] // Remove extra semicolon.  Two because there is a space
+        
+        List<String> args = new ArrayList<>();
+        if (OperatingSystem.current().isWindows()) {
+            args.add("java")
+        }
+        
+        args.add("-classpath")
+        args.add(classpath)
+        args.add("com.snobot.simulator.Main")
+        
+        return args
+    }
+    
+    String addToClasspath(def configurationType, def classpath) {
+        configurationType.dependencies.each {
+            def the_dep = configurationType.files(it)
+            if (!the_dep.isEmpty()) {
+              classpath += the_dep.first().toString() + ";"
+              println "  " + the_dep.first().toString() + ";"
+            }
+            else {
+                println "  UH OH: " + it.toString()
+            }
+        }
+        
+        return classpath
+    }
+
 }
