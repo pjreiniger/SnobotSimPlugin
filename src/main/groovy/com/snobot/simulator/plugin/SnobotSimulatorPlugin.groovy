6e96d06c0cc2e3391d48f317d16082b3d8ce7399
PJ Reiniger <pj.reiniger@gmail.com>
2017-12-16 22:46:45 -0500

    Plugin now unzips native things
    Points to latest version

--- src/main/groovy/com/snobot/simulator/plugin/SnobotSimulatorPlugin.groovy
+++ src/main/groovy/com/snobot/simulator/plugin/SnobotSimulatorPlugin.groovy
@@ -3,13 +3,14 @@ package com.snobot.simulator.plugin
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task
+import org.gradle.api.tasks.Copy;
 import org.gradle.internal.os.OperatingSystem
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class SnobotSimulatorVersionsExtension {
 
-    String snobotSimVersion = "0.4"
+    String snobotSimVersion = "0.6.0"
     String jfreecommon = "1.0.16"
     String jfreechart = "1.0.13"
     String log4j = "1.2.16"
@@ -55,6 +56,11 @@ class SnobotSimulatorPlugin implements Plugin<Project> {
             repo.name = "SnobotSim"
             repo.url = "http://raw.githubusercontent.com/pjreiniger/maven_repo/master/"
         }
+    
+        project.repositories.maven { repo ->
+            repo.name = "PublishedMaven"
+            repo.url = "https://plugins.gradle.org/m2/"
+        }
         
         
         
@@ -69,23 +75,72 @@ class SnobotSimulatorPlugin implements Plugin<Project> {
                 }
             }
         }
+        
+        
+        ////////////////////////////////////////////
+        // Unzip jinput
+        ////////////////////////////////////////////
+        project.configurations
+        {
+            native3rdPartyDeps
+        }
+        project.dependencies 
+        {
+            native3rdPartyDeps 'net.java.jinput:jinput:2.0.7'
+        }
+        project.task("snobotSimUnzipNativeTools", type: Copy, dependsOn: project.configurations.native3rdPartyDeps) { Task task ->
+            task.group = "SnobotSimulator"
+            task.description = "Unzips any 3rd Party native libraries"
+            
+            project.configurations.native3rdPartyDeps.each { Object xxx ->
+                project.copy {
+                    includeEmptyDirs = false
+                    from project.zipTree(xxx)
+                    into "build/native_libs"
+                    include "**/*.dll"
+                    include "**/*.lib"
+                    include "**/*.pdb"
+                    include "**/*.so*"
+                    include "**/*.a"
+                }
+            }
+        }
 
         // Configuration for pulling in everything needed for SnobotSim
         project.dependencies.ext.snobotSimCompile = {
             def l = [
                 "com.snobot.simulator:snobot_sim_gui:${snobotSimVersionExt.snobotSimVersion}:all",
+                "com.snobot.simulator:ctre_override:${snobotSimVersionExt.snobotSimVersion}",
                 "jfree:jcommon:${snobotSimVersionExt.jfreecommon}",
                 "jfree:jfreechart:${snobotSimVersionExt.jfreechart}",
                 "log4j:log4j:${snobotSimVersionExt.log4j}",
                 "org.yaml:snakeyaml:${snobotSimVersionExt.snakeyaml}",
                 "com.miglayout:miglayout-swing:${snobotSimVersionExt.miglayout}",
                 "com.miglayout:miglayout-core:${snobotSimVersionExt.miglayout}",
-                "edu.wpi.first.ntcore:ntcore-jni:${snobotSimVersionExt.ntcore}:all",
-                "edu.wpi.first.cscore:cscore-jni:${snobotSimVersionExt.cscore}:all",
                 "org.opencv:opencv-jni:${snobotSimVersionExt.opencv}:all",
                 "net.java.jinput:jinput:2.0.7",
                 "net.java.jutils:jutils:1.0.0",
             ]
+            
+            if (OperatingSystem.current().isWindows()) 
+            {
+                l += "com.snobot.simulator:snobot_sim_java:${snobotSimVersionExt.snobotSimVersion}:uber_native-windows"
+                l += "com.snobot.simulator:ctre_override:${snobotSimVersionExt.snobotSimVersion}:uber_native-windows"
+            }
+            else 
+            {
+                l += "com.snobot.simulator:snobot_sim_java:${snobotSimVersionExt.snobotSimVersion}:uber_native-linux"
+                l += "com.snobot.simulator:ctre_override:${snobotSimVersionExt.snobotSimVersion}:uber_native-linux"
+            }
+
+            l
+        }
+        
+        project.dependencies.ext.snobotSimJavaCompile = {
+            def l = [
+                "edu.wpi.first.ntcore:ntcore-jni:${snobotSimVersionExt.ntcore}:all",
+                "edu.wpi.first.cscore:cscore-jni:${snobotSimVersionExt.cscore}:all",
+                ]
 
             l
         }
