46943aae5291a7478d910fbfa0aac02856c6e9f8
PJ Reiniger <pj.reiniger@gmail.com>
2019-01-10 21:05:40 -0500

    Fixing default version

--- src/main/groovy/com/snobot/simulator/plugin/SnobotSimulatorVersionExtension.groovy
+++ src/main/groovy/com/snobot/simulator/plugin/SnobotSimulatorVersionExtension.groovy
@@ -8,7 +8,7 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class SnobotSimulatorVersionsExtension {
 
-    String snobotSimVersion = "2019-0.0.1"
+    String snobotSimVersion = "2019-0.0.1-RC"
     String snobotSimCtreVersion = "5.12.0_V0_RC"
     String jfreechart = "1.0.13"
     String jinput = "2.0.7"
